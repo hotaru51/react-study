47a1da157277a79b41f434081b3509e40b769a37
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-07-30 21:53:15 +0900

    chapter4 useEffect

--- chapter_4_1/src/App.jsx
+++ chapter_4_1/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+  console.log('レンダリング')
   const [num, setNum] = useState(0);
+  useEffect(() => alert(), [num])
+
   const onClickButton = () => {
     // state設定用関数には前の値が引数として渡される
     setNum(prev => prev + 1);
