c756ece8109f6ba332d5f0a7d2a9a7d049d4376b
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-07-30 23:16:50 +0900

    chapter5 styled componentsによるCSS適用

--- chapter_5_1/src/App.jsx
+++ chapter_5_1/src/App.jsx
@@ -1,11 +1,13 @@
 import { CssModules } from "./components/CssModules";
 import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
 
 export const App = () => {
   return (
     <>
       <CssModules />
       <StyledJsx />
+      <StyledComponents />
     </>
   );
 };
